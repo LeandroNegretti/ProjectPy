82a096059bc37e472c29ee90bfda94d4ab7f2f49
LeandroNegretti <negrettileandro3@gmail.com>
2024-01-11 19:28:02 -0300

    first commit

---
+++ decrypt.py
@@ -0,0 +1,23 @@
+import os
+from cryptography.fernet import Fernet
+
+def decrypt_file(file_path, key):
+    with open(file_path, "rb") as file: 
+        content = file.read();
+    fernet = Fernet(key);
+    decrypt_content = fernet.decrypt(content)
+    with open(file_path, "wb") as file:
+        file.write(decrypt_content)
+
+def decrypt_files_in_folder(folder_path, key):
+    for root, dils, files in os.walk(folder_path):
+        for file in files:
+            file_path = os.path.join(root, file)
+            decrypt_file(file_path, key)
+
+with open("key.key", "rb") as key_file:
+    key= key_file.read()
+
+folder_path ="C:\\Users\\raque\\OneDrive\\Área de Trabalho\\nomes.teste";
+
+decrypt_files_in_folder(folder_path, key)
---
+++ encrypti.py
@@ -0,0 +1,27 @@
+import os
+from cryptography.fernet import Fernet
+
+def encrypt_file(file_path, key):
+    with open(file_path, "rb") as file: 
+        content = file.read();
+    fernet = Fernet(key);
+    encrypt_content = fernet.encrypt(content)
+    with open(file_path, "wb") as file:
+        file.write(encrypt_content)
+
+def encrypt_files_in_folder(folder_path, key):
+    for root, dils, files in os.walk(folder_path):
+        for file in files:
+            file_path = os.path.join(root, file)
+            encrypt_file(file_path, key)
+
+key = Fernet.generate_key();
+
+folder_path ="C:\\Users\\raque\\OneDrive\\Área de Trabalho\\nomes.teste";
+
+encrypt_files_in_folder(folder_path, key)
+
+with open('key.key', 'wb') as key_file:
+    key_file.write(key)
+
+    
